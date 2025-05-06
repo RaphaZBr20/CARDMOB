ccabb6f9aea1059c896138f2d5e48082bcbf6cd0
Raphael França Ceia <francaceiar@gmail.com>
2025-05-06 07:18:50 -0300

    CRUD simples: addItem

--- Etapa2/app-react-exemplos/App.js
+++ Etapa2/app-react-exemplos/App.js
@@ -1,11 +1,64 @@
+Etapa2/app-react-exemplos/App.js
+
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TextInput} from 'react-native';
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+  
+  /// CRUD em memória
+  const [items, setItems] = useState([]);
+  const [text, setText] = useState('');
+  const [editItemId, setEditItemId] = useState(null);
+  const [editItemText, setEditItemText] = useState('');
+  
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  }
+  
+  const decrementCounter = () => {
+    setCounter(counter - 1);
+  }
+  
+  const addItem = () => {
+    if (text.trim() === '') {
+      return;
+    }
+    const newItem = {
+      id: Math.random().toString(),
+      text: text.trim()
+    }
+    setItems([...items, newItem]);
+    setText('');
+    console.log(items);
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput 
+        style={StyleSheet.input}
+        value={text}
+        onChangeText={setText}
+        placeholder='Enter text item'
+      />
+      <Button
+      title="Add Item"
+      onPress={addItem}
+      />
+      <Text style={styles.text}>teste!</Text>
+      <Image
+        source={{uri:"https;//picsum.photos/200"}}
+        style={{width: 200, height: 200}}
+        />
+
       <StatusBar style="auto" />
+      <Text style={styles.text}>Counter: {counter}</Text>
+      
+      <View style={styles.buttonContainer}>
+        <Button title='Increment' onPress={incrementCounter} />
+        <Button title='Dencrement' onPress={decrementCounter} />
+      </View>
     </View>
   );
 }
@@ -17,4 +70,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 24,
+  },
+  buttonContainer: {
+    flexDirection: 'row'
+  }
 });
