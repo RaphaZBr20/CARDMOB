f15e6556895ed537190ec164daf77024c00b9919
Raphael <francaceiar@gmail.com>
2025-08-19 08:40:34 -0300

    Inclui novas telas: registro e login

--- Etapa3/loja-react-app/src/navigation/RootNavigator.tsx
+++ Etapa3/loja-react-app/src/navigation/RootNavigator.tsx
@@ -7,6 +7,8 @@ import { RootStackParamList, TabParamList } from './types';
 // Telas do app.
 import HomeScreen from "../screens/HomeScreen";
 // importar depois que implementar: DetailsScreen, SettingsScreen
+import RegisterScreen from "../screens/RegisterScreen";
+import LoginScreen from "../screens/LoginScreen";
 
 const AppStack = createNativeStackNavigator<RootStackParamList>();
 const Tab = createBottomTabNavigator<TabParamList>();
@@ -16,6 +18,7 @@ function TabNavigator() {
         <Tab.Navigator>
             <Tab.Screen name="Home" component={HomeScreen}/>
             <Tab.Screen name="Settings" component={HomeScreen} />
+            <Tab.Screen name="Register" component={RegisterScreen} />
         </Tab.Navigator>
     );
 }
@@ -26,12 +29,17 @@ function AppNavigator() {
             <AppStack.Screen 
               name="Tabs"
               component={TabNavigator}
-              options={{ headerShown: false}}
+              options={{ headerShown: false }}
             />
             <AppStack.Screen 
               name="Details"
               component={HomeScreen}
               options={{ title: 'Detalhes' }}
+            />
+            <AppStack.Screen
+              name="Login"
+              component={LoginScreen}
+              options={{ title: "Acessar" }}
             />
         </AppStack.Navigator>
     );
