b549456a2a68dff93fb8e6af3381732319911049
Raphael <francaceiar@gmail.com>
2025-10-07 08:10:18 -0300

    Corrigi ação de remover do carrinho

--- Etapa3/loja-react-app/src/screens/cart/CartItem.tsx
+++ Etapa3/loja-react-app/src/screens/cart/CartItem.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
-import{ useShop } from "../../contexts/ShopContext";
+import { useShop } from "../../contexts/ShopContext";
+
 const CartItem = ({ item }: any) => {
     // @todo implementar o context para persistir dados do carrinho.
+    const { addToCart, removeFromCart } = useShop();
 
-    const { addToCart } = useShop();
-
-    const handleRemove = () => {
+    const handleRemove = (item: any) => {
+        removeFromCart(item.id);
         console.log('exclui produto');
     }
 
@@ -18,7 +19,7 @@ const CartItem = ({ item }: any) => {
                 <Text style={styles.name}>{item.name}</Text>
                 <View style={styles.quantity}>
                     <Text style={styles.price}>R$ {(item.price * item.quantity).toFixed(2)}</Text>
-                    <TouchableOpacity onPress={() => addToCart(item)} style={styles.button}>
+                    <TouchableOpacity onPress={() => addToCart(item, -1)} style={styles.button}>
                         <Text style={styles.buttonText}>-</Text>
                     </TouchableOpacity>
                     <Text style={styles.quantityValue}>{item.quantity}</Text>
@@ -27,7 +28,7 @@ const CartItem = ({ item }: any) => {
                         <Text style={styles.buttonText}>+</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity onPress={() => handleRemove()} style={styles.button}>
+                    <TouchableOpacity onPress={() => handleRemove(item)} style={styles.button}>
                         <Text style={styles.buttonText}>Remover</Text>
                     </TouchableOpacity>
                 </View>
